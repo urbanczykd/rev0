740d27b678c90c4c09def355fd07183f160f4561
urbanczykd <urbanczykd@gmail.com>
2014-01-14 11:23:10 +0100

    grid

--- spec/spec_helper.rb
+++
@@ -1,3 +0,0 @@
-require_relative '../revoo.rb'
-
-require 'yaml'
---
+++ test1/grid.rb
@@ -0,0 +1,30 @@
+
+ARR = ("a".."z").to_a + ("A".."Z").to_a
+SIZE = ARR.size
+
+def letter
+	ARR[rand(SIZE)]
+end
+
+def line(len)
+	line = Array.new
+	len.times do
+		line << letter
+	end
+	line.join(" ")
+end
+
+def gen_grid(x, y)
+	y.times{ puts line(x) }
+end
+
+def close
+	puts "Please provide 2 arguments."
+end
+
+if ARGV[0] && ARGV[1]
+	gen_grid(ARGV[0].to_i, ARGV[1].to_i)
+else
+	close
+end
+
