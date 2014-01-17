4e05420fa7875c5cd6bffcba2a748b1f2bccab32
urbanczykd <urbanczykd@gmail.com>
2014-01-17 12:32:00 +0100

    sponsor pay

---
+++ sspay/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sspay::Application.config.secret_token = '104255e6b9f62aac6b059dff43b3fbd9d7ae771b19fb86aab3a64e111e19295a611619ed450ac6891da398e6dae496e58371dad9e4c68ec44494e5a0a92a809a'
---
+++ sspay/config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Sspay::Application.config.session_store :cookie_store, key: '_sspay_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Sspay::Application.config.session_store :active_record_store
